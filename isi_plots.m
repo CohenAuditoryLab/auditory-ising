6b33299bdd851d43768ba53f804fa8db63c6128e
Brianna <brianna.karpowicz@gmail.com>
2018-07-19 15:53:04 -0400

    updated for total violation plot

--- isi_plots.m
+++ isi_plots.m
@@ -1,4 +1,4 @@
-function isi_plots(directory) 
+function all_violations = isi_plots(directory)
 
 %create folder to save plots
 mkdir(directory, 'isi_plots');
@@ -9,37 +9,61 @@ CohenNeurons = CohenNeurons.CohenNeurons;
 
 num_neurons = size(CohenNeurons, 2);
 
+all_violations = zeros(1, num_neurons);
+
 %for each neuron
 for n = 1:num_neurons
     disp(['-------NEURON ' num2str(n) ' -------']);
     neuron_num = CohenNeurons(n).ID;
     
-    %get all spike times for all trials 
+    %get all spike times for all trials
     neuron = CohenNeurons(n);
-    t = {neuron.trials(:).spike_times};
+    t = {neuron.trials(:).spikes};
     num_trials = size(t, 2);
     
     %calculate interspike intervals for each trial
     isi = [];
     for m = 1:num_trials
-        d = diff(t{m});
-        isi(end+1:end+length(d)) = d;
-    end 
+        spike_t = t{m};
+        spike_t = spike_t(spike_t > 0);
+        d = diff(spike_t);
+        isi = horzcat(isi, d);
+    end
+    
+    violation_rate = sum(isi < 3) / length(isi);
+    all_violations(n) = violation_rate;
     
     %plot histogram
-    figure();
-    h = histogram(isi);
-    title(['Interspike Interval Histogram, Neuron ' num2str(neuron_num)]);
-    xlabel('Interval (ms)');
-    ylabel('Frequency');
-    set(gca, 'FontSize', 10);
+%     figure();
+%     h = histogram(isi, 0:5:1000);
+%     title(['Interspike Interval Histogram, Neuron ' num2str(neuron_num) ...
+%         '; Violation Rate: ' num2str(violation_rate*100) '%']);
+%     xlabel('Interval (ms)');
+%     ylabel('Frequency');
+%     set(gca, 'FontSize', 8);
     
-    %save 
-    filename = [directory filesep 'isi_plots' filesep 'isi_neuron_' num2str(neuron_num)];
-    print(filename, '-dpng');
-end 
+    %save
+%     filename = [directory filesep 'isi_plots' filesep 'isi_neuron_' num2str(neuron_num)];
+%     print(filename, '-dpng');
+end
 
 %close all the figures
 close all;
 
-end 
+labels = categorical(cell2mat({CohenNeurons(:).ID}));
+bar(labels, all_violations)
+
+end
+
+% function plotRaster(tVec)
+% hold all;
+% for trialCount = 1:size(tVec,2)
+%     spikePos = tVec{trialCount};
+%     spikePos = spikePos(spikePos > 0);
+%     for spikeCount = 1:length(spikePos)
+%         plot([spikePos(spikeCount) spikePos(spikeCount)], ...
+%             [trialCount-0.4 trialCount+0.4], 'k');
+%     end
+% end
+% ylim([0 size(tVec, 2)+1]);
+% end 
