d9b7d2e53190f2acb176f8b9db922195c9ac25ca
Matt Schaff <matthew.schaff@gmail.com>
2018-09-26 11:35:28 -0400

    ACE/running/runACEforDomoData_NewBark_20180926.m

---
+++ ACE/running/runACEforDomoData_NewBark_20180926.m
@@ -0,0 +1,25 @@
+% 9/26/2018
+
+% PURPOSE OF THIS FILE
+    % to run ACE algorithm on custom generated *.p file from Domo data
+    
+% create output dir
+    base_output = '/home/matt/Documents/Ising/output/ACE_NewBark/'; 
+    output = [base_output '20180926_10percent'];
+    if (exist(output, 'dir') == 0)
+        mkdir(output);
+    end
+% generate .p file
+    addpath(genpath('../'));
+    data = '../../../data/ACE/201800807_Voc_Ripple/final_clusters.mat';
+    bin_size = 20e-3;
+    time_units = 1e-3;
+    generateACEinputSpikeTimes(data, time_units, bin_size, output, .1);
+% add ACE path
+    ACE_path = '/home/matt/Documents/ACE';
+    addpath(genpath(ACE_path));
+% run ACE algorithm
+    setenv('LD_LIBRARY_PATH', '/usr/lib/x86_64-linux-gnu/libstdc++.so.6');
+    system([ACE_path '/bin/ace -d ' output ' -i ACEinput -o ACEinput-out -b 5961']);
+% run qls
+    system([ACE_path '/bin/qls -d ' output ' -i ACEinput-out -o ACEinput-out-learn -c ACEinput']);
