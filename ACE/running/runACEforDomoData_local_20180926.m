5d5cf6380665b06329fabd6cc4759ecb43a7a471
Matt Schaff <matthew.schaff@gmail.com>
2018-09-27 14:08:40 -0400

    ACE/running/runACEforDomoData_local_20180926.m -- output frequency pattern graph

--- ACE/running/runACEforDomoData_local_20180926.m
+++ ACE/running/runACEforDomoData_local_20180926.m
@@ -23,7 +23,7 @@
             
     % let's make some graphs
         % J_ij after learning
-        [h, j_matrix] = extractFittedParameters('/Users/mschaff/Documents/DISSERTATION/Ising/data/ACE/201800807_Voc_Ripple/finalclusters.mat','/Users/mschaff/Documents/DISSERTATION/Ising/output/ACE_NewBark/20180921/ACEinput-out-learn.j');
+        [h, j_matrix] = extractFittedParameters(20,'/Users/mschaff/Documents/DISSERTATION/Ising/output/ACE_NewBark/20180921/ACEinput-out-learn.j');
         f = heatmap(j_matrix);
         title('J_ij -- After Learning');
         saveas(gcf,[output_dir filesep 'Jij_afterlearning.jpg']);
@@ -48,4 +48,23 @@
         saveas(gcf,[output_dir filesep 'Custom_J_ij.jpg']);
         close all;
         
+   % generate spike train files for 10% of data
+        output_dir = '/Users/mschaff/Documents/DISSERTATION/Ising/output/ACE_local/20180926/10_percent';
+        generateACEinputSpikeTimes(data, time_units, bin_size, output_dir, .1);
+        
+   % I then ran ACE on New Bark with the first 10% of the data
+   % must now plot predictions
+        output_dir = '/Users/mschaff/Documents/DISSERTATION/Ising/output/ACE_NewBark/20180926_10percent';
+        [h, j_matrix] = extractFittedParameters(20,[output_dir filesep 'ACEinput-out-learn.j']);
+        [sigm, states]  = sample_ising_exact(h', j_matrix);
+        load([output_dir filesep 'spikes_by_bin.mat']);
+        spikes_by_bin(spikes_by_bin==0) = -1;
+        neuron_trains = mat2cell(spikes_by_bin, ones([1 size(spikes_by_bin,1)]), [size(spikes_by_bin,2)]);
+        filepath = [output_dir filesep 'neuron_trains.mat'];
+        save(filepath, 'neuron_trains');
+        BoltzmannMetrics(output_dir);
+        test_logical = ones([1 size(spikes_by_bin,2)]);
+        plot_num_firing(h', j_matrix, test_logical, filepath);
+        k=10;
+        pattern_frequencies_subset(h0, J, k, test_logical, output_dir);
         
