770aa8b677140836ad609d21f006a65fe12d7671
Matt Schaff <matthew.schaff@gmail.com>
2018-10-01 12:41:49 -0400

    ACEpipeline.m that runs ACE & generates figures

---
+++ ACE/methods/ACEpipeline.m
@@ -0,0 +1,31 @@
+function ACEpipeline(data_path, output_dir, time_units, bin_size, use_chunks, ACE_path)
+% ACEpipeline - runs ACE and generates result files & figures
+    % input variables:
+        % data_path 
+            % path to *.mat file with a variable g 
+            % g - (2xNumber of spikes), 
+                % column 1 - neuron ID 
+                % column 2 - spike times
+        % output_dir
+            % (string) directory where output should be saved
+        % time_units
+            % (double) units in time of the data (e.g. 1e-3, which would correspond
+            % to ms)
+        % bin_size
+            % (double) size of bin in seconds (e.g. 20e-3, or 20 ms bins)
+        % use_chunks
+            % (boolean) whether to chunk the data by 10,000 bin chunks
+            % note - ACE seems to fail when you give it more than 10k bins
+        % ACE_path
+            % (string) where ACE is
+
+  %% LOGIC
+    % add all code to path
+        addpath(genpath(fileparts(fileparts(fileparts(mfilename('fullpath'))))));
+    % generate ACE input files, with a training + test split
+        generateACEinputSpikeTimes(data_path, time_units, bin_size, output_dir,use_chunks); 
+    % run ACE & learning algorithm on the *.p file
+        runACEonCohenData(output_dir, ACE_path);     
+    % extract h & J parameters from *.j file & generate figures
+        plotACEresult(output_dir);
+end
