48a7180d78807aff6b687df7e02d1bb38357dddd
Matt Schaff <matthew.schaff@gmail.com>
2018-10-03 16:47:01 -0400

    I can run ACE pipeline on bird data now

---
+++ ACE/methods/birds/ACEpipeline_birds.m
@@ -0,0 +1,33 @@
+function ACEpipeline_birds(data_path, output_dir, run_custom, ACE_path)
+% ACEpipeline - runs ACE and generates result files & figures
+    % input variables:
+        % data_path 
+            % path to *.mat file with a variable g 
+            % g - (2xNumber of spikes), 
+                % column 1 - neuron ID 
+                % column 2 - spike times
+        % output_dir
+            % (string) directory where output should be saved
+        % run_custom
+            % (boolean) whether to also run custom Ising algorithm
+        % ACE_path
+            % (string) where ACE is
+
+  %% LOGIC
+    % set up vars
+        if exist('indep_c_ij', 'var') == 0
+          indep_c_ij = false;
+        end
+    % add all code to path
+        addpath(genpath(fileparts(fileparts(fileparts(mfilename('fullpath'))))));
+    % generate ACE input files, with a training + test split
+        generateACEinput_birds(data_path, output_dir); 
+    % run ACE & learning algorithm on the *.p file
+        runACEonCohenData(output_dir, ACE_path);     
+    % extract h & J parameters from *.j file & generate figures
+        plotACEresult(output_dir);
+    % if custom_algorithm should be run, run it
+        if (run_custom) 
+            runPlotCustomAlgorithm(output_dir);
+        end
+end
