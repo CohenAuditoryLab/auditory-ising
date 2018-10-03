c1651473662f659436f52e04c057d4159e5849c5
Matt Schaff <matthew.schaff@gmail.com>
2018-10-03 16:47:18 -0400

    running_20181003.m

---
+++ ACE/running/local/running_20181003.m
@@ -0,0 +1,9 @@
+% 10/03/2018
+
+% PURPOSE OF THIS FILE
+    % to run ACE pipeline on bird data
+        output_dir = '/Users/mschaff/Documents/DISSERTATION/Ising/output/ACE_local/20181003_bird';
+        ACE_path = '/Users/mschaff/Documents/REPOS/ACE';
+        run_custom =1;
+        data_path = '/Users/mschaff/Documents/DISSERTATION/Ising/data/ACE/Eve_birds/data_allBirds_binary_2010M6.txt';
+        ACEpipeline_birds(data_path, output_dir, run_custom, ACE_path);
