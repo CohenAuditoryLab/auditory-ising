7c3ccf60a9d237eb4859b2b1672da198fd207a87
Matt Schaff <matthew.schaff@gmail.com>
2018-10-11 18:18:13 -0400

    rest of Vijay's tests

--- ACE/running/tests/vijay_tests.m
+++ ACE/running/tests/vijay_tests.m
@@ -34,7 +34,7 @@
         disp(['Negative Js: (Eve) ' num2str(sum(J < 0)) ', (Matt) ' num2str(sum(J_matt < 0))]);
 %% measure how sparse J are after mu=1/bins
     % already ran this
-        parameters = importdata([core_dir '/output/ACE_local/20181010_all/201801010_DOMO/chunk_1__10000/ACEinput-out.j']);
+        parameters = importdata([core_dir '/output/ACE_local/20181010_all/201801010_DOMO_fixedtrain/chunk_1__10000/ACEinput-out.j']);
         numneurons = 20;
         J = parameters(numneurons+1:end);
         disp('---- Sparsity of J: mu=1/bins ----');
@@ -43,5 +43,73 @@
         disp(['Negative Js: ' num2str(sum(J <0)) '/' num2str(numel(J))]);
 
 %% measure how sparse J are after mu=1
-
+    % base output
+        base_output = [core_dir '/output/ACE_local/20181010_all'];
+    % move 1st chunk & delete ACE output
+        destination = [base_output filesep '201801010_DOMO_test_reg_1'];
+        mkdir(destination);
+        source = [base_output filesep '201801010_DOMO_fixedtrain' filesep 'chunk_1__10000'];
+        copyfile(source, destination);
+        if (exist([destination filesep 'figures'], 'dir')) 
+            rmdir([destination filesep 'figures'], 's');
+        end
+        if (exist([destination filesep 'figures_custom'], 'dir')) 
+            rmdir([destination filesep 'figures_custom'], 's');
+        end
+        file = dir(fullfile(destination, '*.j'));
+        if (~isempty(file))
+            delete([destination filesep file.name]);
+        end
+        file = dir(fullfile(destination, '*.sce'));
+        if (~isempty(file))
+            delete([destination filesep file.name]);
+        end
+    % run ACE w/ regularization of 1
+        ACE_path = '/Users/mschaff/Documents/REPOS/ACE';
+        regularization = '1e0';
+        %runACEonCohenData(destination, ACE_path, regularization);
+    %forgot to create the train_logical.mat file
+        load([destination filesep 'test_logical.mat']);
+        train_logical = ~test_logical;
+        save([destination filesep 'train_logical.mat'], 'train_logical');
+        runACEonCohenData(destination, ACE_path, regularization);
+    % extract parameters & display results
+        parameters = importdata([destination filesep 'ACEinput-out.j']);
+        numneurons = 20;
+        J = parameters(numneurons+1:end);
+        disp('---- Sparsity of J: mu=1 ----');
+        disp(['Nonzero Js: ' num2str(sum(J ~=0)) '/' num2str(numel(J))]);
+        disp(['Positive Js: ' num2str(sum(J >0)) '/' num2str(numel(J))]);
+        disp(['Negative Js: ' num2str(sum(J <0)) '/' num2str(numel(J))]);
 %% measure how sparse J are after mu=0
+    %move 1st chunk & delete ACE output
+        destination = [base_output filesep '201801010_DOMO_test_reg_0'];
+        mkdir(destination);
+        source = [base_output filesep '201801010_DOMO_test_reg_1'];
+        copyfile(source, destination);
+        if (exist([destination filesep 'figures'], 'dir')) 
+            rmdir([destination filesep 'figures'], 's');
+        end
+        if (exist([destination filesep 'figures_custom'], 'dir')) 
+            rmdir([destination filesep 'figures_custom'], 's');
+        end
+        file = dir(fullfile(destination, '*.j'));
+        if (~isempty(file))
+            delete([destination filesep file.name]);
+        end
+        file = dir(fullfile(destination, '*.sce'));
+        if (~isempty(file))
+            delete([destination filesep file.name]);
+        end
+    % run ACE w/ regularization of 1
+        ACE_path = '/Users/mschaff/Documents/REPOS/ACE';
+        regularization = '0';
+        runACEonCohenData(destination, ACE_path, regularization);
+    % extract parameters & display results
+        parameters = importdata([destination filesep 'ACEinput-out.j']);
+        numneurons = 20;
+        J = parameters(numneurons+1:end);
+        disp('---- Sparsity of J: mu=0 ----');
+        disp(['Nonzero Js: ' num2str(sum(J ~=0)) '/' num2str(numel(J))]);
+        disp(['Positive Js: ' num2str(sum(J >0)) '/' num2str(numel(J))]);
+        disp(['Negative Js: ' num2str(sum(J <0)) '/' num2str(numel(J))]);
