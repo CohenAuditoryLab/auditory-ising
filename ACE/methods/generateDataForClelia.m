3fbd9a46023fd848975ec422cafd81c6718a57f0
Matt Schaff <matthew.schaff@gmail.com>
2018-12-26 09:24:00 -0500

    generateDataForClelia.m - method to generate data for Clelia

---
+++ ACE/methods/generateDataForClelia.m
@@ -0,0 +1,15 @@
+function generateDataForClelia(data, output_dir)
+    % get matrix of spike trains by neuron
+    load(data);
+    % invert matrix 
+    a = spikes_by_bin.';
+    % get placeholder text string to put in fprint
+    placeholder_string = '';
+    for i=1:size(a,2)
+        placeholder_string = [placeholder_string '%1g'];
+    end
+    % save file
+    fid = fopen([output_dir filesep 'dataForCelia.txt'], 'wt');
+    fprintf(fid,[placeholder_string '\n'],a);
+    fclose(fid);
+end
