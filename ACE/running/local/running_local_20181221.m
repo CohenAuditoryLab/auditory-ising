ac06b7139deef18d54d1b659b0d48a46055c5bd6
Matt Schaff <matthew.schaff@gmail.com>
2018-12-26 09:22:50 -0500

    generate output for Clelia from bad dataset running_local_20181221.m

--- ACE/running/local/running_local_20181221.m
+++ ACE/running/local/running_local_20181221.m
@@ -35,3 +35,24 @@
     data_path = [core_dir filesep 'data' filesep 'ACE' filesep '20180711_Vocalization_d01' filesep 'ALL_Domo_20180711_Vocalization_d01_finalclusters.mat'];
     multipleRunACE(data_path, output_dir, time_units, bin_size, ACE_path, mc_algorithm_path, p_train);
     distributionJSDmultipleRunACE(output_dir);
+
+%% generate file format for Clelia of bad dataset
+
+    % get matrix of spike trains by neuron (21 Neurons X 1192108 ms time bins)
+    data = [core_dir filesep 'data' filesep 'ACE' filesep '20180820_Ripple2_d03' filesep 'WCSpikeSorting_20180820_Ripple2_d03_finalclusters.mat'];
+    load(data);
+    % invert matrix (1192108 ms time bins X 21 Neurons)
+    a = spikes_by_bin.';
+    % get placeholder text string to put in fprint
+    placeholder_string = '';
+    for i=1:size(a,2)
+        placeholder_string = [placeholder_string '%1g'];
+    end
+    % save file
+    output_dir = [base_output filesep '20180820_Ripple2_d03_dataForClelia'];
+    if (exist(output_dir, 'dir') == 0)
+        mkdir(output_dir);
+    end
+    fid = fopen([output_dir filesep '20180820_Ripple2_d03_dataForClelia.txt'], 'wt');
+    fprintf(fid,[placeholder_string '\n'],a);
+    fclose(fid);
