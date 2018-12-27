4af23e95fcfc79a40ce146b449d0d31fcd6ca53e
Matt Schaff <matthew.schaff@gmail.com>
2018-12-27 09:46:23 -0500

    firing rates figure in running_local_20181226.m

--- ACE/running/local/running_local_20181226.m
+++ ACE/running/local/running_local_20181226.m
@@ -31,3 +31,38 @@
     bin_size = 1e-3;
     generateACEinputSpikeTimes(data, time_units, bin_size, output_dir, chunk, indep_c_ij, chunk_size, p_train);
     generateDataForClelia(spikes_data, output_dir);
+    
+%% look at firing rates of good & bad dataset
+    % make dir
+        investigate_dir = [base_output filesep 'good_vs_bad_data'];
+        if (exist(investigate_dir, 'dir') == 0)
+            mkdir(investigate_dir);
+        end
+    % package data
+        setStandardACEParams; chunk = false; chunk_size = 1e4;
+        
+        % package bad data
+            data = [core_dir filesep 'data' filesep 'ACE' filesep '20180820_Ripple2_d03' filesep 'WCSpikeSorting_20180820_Ripple2_d03_finalclusters.mat'];
+            output_dir = [investigate_dir filesep '20180820_Ripple2_d03'];
+            generateACEinputSpikeTimes(data, time_units, bin_size, output_dir, chunk, indep_c_ij, chunk_size, p_train);
+            
+        % package good data
+            data = [core_dir filesep 'data' filesep 'ACE' filesep '20180807_Ripple2_d01' filesep 'Domo_20180807_Ripple2_d01_finalclusters.mat'];
+            output_dir = [investigate_dir filesep '20180807_Ripple2_d01'];
+            generateACEinputSpikeTimes(data, time_units, bin_size, output_dir, chunk, indep_c_ij, chunk_size, p_train);
+            
+        % get bad package & good package
+            good_spikes_by_bin = load([investigate_dir filesep '20180807_Ripple2_d01' filesep 'spikes_by_bin.mat']);
+            good_spikes_by_bin = good_spikes_by_bin.spikes_by_bin;
+            bad_spikes_by_bin = load([investigate_dir filesep '20180820_Ripple2_d03' filesep 'spikes_by_bin.mat']);
+            bad_spikes_by_bin = bad_spikes_by_bin.spikes_by_bin;
+        
+     % firing rates for all neurons in bad dataset
+        firing_rate_graph(bad_spikes_by_bin, bin_size, [investigate_dir filesep '20180820_Ripple2_d03']);
+    
+    % firing rates for all neurons in good dataset
+        firing_rate_graph(good_spikes_by_bin, bin_size, [investigate_dir filesep '20180807_Ripple2_d01']);
+        
+        
+        
+        
